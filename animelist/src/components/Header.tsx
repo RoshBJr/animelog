d50e88f6295b37f9533f22c9ba28b91e6b3e357f
RoshBJr <roshanjr3@gmail.com>
2023-01-28 14:11:02 -0500

    peut maintenant changer entre home et my list

--- animelist/src/components/Header.tsx
+++ animelist/src/components/Header.tsx
@@ -1,14 +1,30 @@
 import './css/header.css';
+import { useState } from 'react';
+import ListeDesAnimes from './ListeDesAnimes';
+import MyListDesAnimes from './MyListDesAnimes';
 
 export default function Header() {
 
+    const [PageComponent, setPageComponent] = useState(<ListeDesAnimes/>);
+
+    function ReturnHome() {
+        setPageComponent(<ListeDesAnimes/>);
+    }
+
+    function MyList() {
+        setPageComponent(<MyListDesAnimes/>);
+    }
+
     return(
-        <header className="headerNav">
-            <h1>AnimeLoG</h1>
-            <nav>
-                <span>Home</span>
-                <span>My List</span>
-            </nav>
-        </header>
+        <>
+            <header className="headerNav">
+                <h1>AnimeLoG</h1>
+                <nav>
+                    <button onClick={ReturnHome}>Home</button>
+                    <button onClick={MyList}>My list</button>
+                </nav>
+            </header>
+            {PageComponent}
+        </>
     );
 }
---
+++ animelist/src/components/MyListDesAnimes.tsx
@@ -0,0 +1,8 @@
+export default function MyListDesAnimes() {
+    
+    return (
+        <div>
+            <h1>my anime list</h1>
+        </div>
+    );
+}
