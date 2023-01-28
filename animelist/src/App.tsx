2cb7b99dc5d6d04f2ca187be65a9050e8e628eec
RoshBJr <roshanjr3@gmail.com>
2023-01-28 14:13:08 -0500

    useState pour naviguer vers my list et home

--- animelist/src/App.tsx
+++ animelist/src/App.tsx
@@ -3,13 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import ListeDesAnimes from './components/ListeDesAnimes';
+import MyListDesAnimes from './components/MyListDesAnimes';
 import './components/css/color.css';
+import { useState } from 'react';
 
 function App() {
+
   return (
     <div className="App">
       <Header/>
-      <ListeDesAnimes/>
     </div>
   );
 }
